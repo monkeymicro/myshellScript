966838549ee75c2b9c085c6a18b8ed6e1d23c082
monkeymicro <cutecat2023@163.com>
2024-01-02 19:00:25 -0800

    finish example1

--- cmake_qt6_console_app/main.cpp
+++ cmake_qt6_console_app/main.cpp
@@ -9,4 +9,8 @@ using namespace std;
 int main(){
     cout << "hello world!!!"<< endl;
     qDebug() << "qt say hello" ;
+    Mat src = Mat(314, 314, CV_8U);//UINT
+    randu(src, Scalar::all(0), Scalar::all(255));
+	imshow("random images", src);
+	waitKey(0);
 }
